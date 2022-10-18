c03987cc615f2622b0e96ccdc3c9adc8a767971e
Luis Boscan <lboscannava@gmail.com>
2022-10-17 21:35:06 -0300

    Additional field added

--- backend/activitylogs/api/serializers.py
+++ backend/activitylogs/api/serializers.py
@@ -4,6 +4,8 @@ from activitylogs.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    likes = serializers.IntegerField(default=0)
+
     class Meta:
         model = Post
-        fields = ("id", "title", "description", "image_src")
+        fields = ("id", "title", "description", "image_src", "likes")
--- backend/activitylogs/api/views.py
+++ backend/activitylogs/api/views.py
@@ -1,9 +1,16 @@
+from django.db.models import Count, Q
 from rest_framework import viewsets
 
 from activitylogs.api.serializers import PostSerializer
-from activitylogs.models import Post
+from activitylogs.models import Post, ActivityLog
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes=Count(
+            "actions",
+            distinct=True,
+            filter=Q(actions__interaction_type=ActivityLog.InteractionType.LIKE),
+        )
+    )
     serializer_class = PostSerializer
