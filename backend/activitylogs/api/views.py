502654c6a2e1120928a5438033673af9ab4d336c
Luis Boscan <lboscannava@gmail.com>
2022-10-17 22:21:36 -0300

    Stats added

--- backend/activitylogs/api/views.py
+++ backend/activitylogs/api/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.models import User
 from django.db.models import Count, Q
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from activitylogs.api.serializers import PostSerializer
 from activitylogs.models import Post, ActivityLog
@@ -25,3 +28,29 @@ class PostViewSet(viewsets.ModelViewSet):
                        Q(actions__user=user),
             )
         )
+
+    @action(
+        detail=False,
+        permission_classes=(IsAuthenticated,),
+        methods=("get",)
+    )
+    def stats(self, request):
+        data = {
+            "registered_users": self._registered_users(),
+            "top_most_viewed": self._top_posts(ActivityLog.InteractionType.VIEW),
+            "top_most_liked": self._top_posts(ActivityLog.InteractionType.LIKE)
+        }
+        return Response(data)
+
+    def _registered_users(self):
+        return User.objects.count()
+
+    def _top_posts(self, interaction_type, top=5):
+        posts = Post.objects.annotate(
+            total=Count(
+                "actions",
+                distinct=True,
+                filter=Q(actions__interaction_type=interaction_type)
+            )
+        ).order_by('total')[:top]
+        return posts.values('title', 'total')
